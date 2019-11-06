18c3b81bf4e9b17967e292f0f124ce9b097e1fdb
airton47 <3002614110101@ingenieria.usac.edu.gt>
2019-11-06 12:00:27 -0600

    Interfaz grafica para usuarios de la aplicacion

---
+++ [ORGA]Practica_04/src/orga/practica_04/ORGAPractica_04.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package orga.practica_04;
+
+/**
+ *
+ * @author aiyel
+ */
+public class ORGAPractica_04 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        //Instancia de la ventana principal del juego:
+        
+    }
+    
+}
